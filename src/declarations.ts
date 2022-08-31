0d2b607c70c7d52d3e7af4b5bfb5bf46cc2f9863
Mark Tang <MarkTangCd@hotmail.com>
2022-08-31 16:45:12 +0800

    done

---
+++ src/declarations.ts
@@ -0,0 +1,16 @@
+interface Ethereum {
+  send: unknown
+  enable: () => Promise<string[]>
+  on?: (method: string, listener: (...args: any[]) => void) => void
+  removeListener?: (method: string, listener: (...args: any[]) => void) => void
+}
+
+interface Coin98 {
+  provider: Ethereum
+}
+
+declare interface Window {
+  coin98: Coin98
+}
+
+declare const __DEV__: boolean
